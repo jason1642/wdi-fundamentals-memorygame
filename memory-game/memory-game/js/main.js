e6af8d5a27b9401f3ddd9c566f6648fb46e4ea8a
Jason Cruz <jason1642@gmail.com>
2020-02-01 19:59:20 -0500

    created game logic

--- memory-game/memory-game/js/main.js
+++ memory-game/memory-game/js/main.js
@@ -1,5 +1,14 @@
-let cardOne = "Queen";
-let cardTwo = "Queen";
-let cardThree = "King";
-let cardFour = "King";
-console.log("User flipped " + cardOne);
+let cards = ["Queen", "Queen", "King", "King"];
+let cardsInPlay = [];
+let cardOne = cards[0];
+let cardTwo = cards[1];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+// console.log("User flipped " + );
+if(cardsInPlay.length === 2){
+    if(cardsInPlay[0] === cardsInPlay[1]){
+        alert("You found a match!");
+    } else{
+        alert("Sorry, try again.");
+    }
+}
